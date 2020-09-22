fd9b5de8b1975565378c10df94be4fba6e67db41
Otuokere Tobechukwu <Otuokere@users.noreply.github.com>
2020-09-22 02:54:33 +0100

    Done implementing Authentication and authorization

--- trill/settings.py
+++ trill/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'trill.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'account/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'account/static')
+# ]
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
